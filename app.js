06525cf02b9b1a3893fa4d43c1a074eae92f86a6
MrBusiness87 <bbartula87@gmail.com>
2020-07-23 07:17:31 -0400

    07/23/2020-added more code hoping that it would fix the issue with the next test, it did not, still working on second test.

--- app.js
+++ app.js
@@ -8,55 +8,113 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+const {
+  defineEmployee,
+  employeeQuestions,
+  managerQuestions,
+  engineerQuestions,
+  internQuestions,
+} = inquire;
+
 const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
-inquirer
-  .prompt([{
-      type: "input",
-      message: "What is your position?",
-      name: "position",
-    },
-    {
+const inquire = inquirer.prompt({
+  defineEmployee: [{
+    type: "list",
+    message: "Enter Personnel:",
+    name: "userChoice",
+    choices: ["Engineer", "Intern", "Complete"]
+  }],
+  employeeQuestions: [{
       type: "input",
       message: "What is your name?",
-      name: "name",
+      name: "name"
     },
     {
       type: "input",
-      message: "What is your ID number?",
+      message: "Please enter your employee ID number:",
       name: "id",
     },
     {
       type: "input",
-      message: "What is your email address?",
+      message: "Please enter a valid email:",
       name: "email",
-    },
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "gitname",
-    },
-  ]);
-// and to create objects for each team member (using the correct classes as blueprints!)
+    }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+  ],
+  managerQuestions: [{
+    type: "input",
+    message: "Office Number",
+    name: "officeNumber",
+  }],
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+  engineerQuestions: [{
+    type: "input",
+    message: "GitHub Username",
+    name: "githubUsername"
+  }],
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+  internQuestions: [{
+    type: "input",
+    message: "School",
+    name: "school"
+  }]
+});
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const htmlRenderer = require('./lib/htmlRenderer');
+
+const employeeArray = [];
+
+function init() {
+  inquirer
+    .prompt(employeeQuestions.concat(managerQuestions))
+    .then(({
+      name,
+      id,
+      email,
+      officeNumber
+    }) => {
+      let manager = new Manager(name, id, officeNumber, email);
+      employeeArray.push(manager);
+      getEmployee();
+    })
+}
+
+function getEmployee() {
+  inquirer
+    .prompt(defineEmployee)
+    .then(({
+      userChoice
+    }) => {
+      switch (userChoice) {
+        case 'Engineer':
+          getEngineer();
+          break;
+        case 'Intern':
+          getIntern();
+          break;
+        case 'Complete':
+          htmlRenderer(employeeArray);
+          break;
+      }
+    })
+}
+
+function getIntern() {
+  inquirer
+    .prompt(employeeQuestions.concat(internQuestions))
+    .then(({
+      name,
+      id,
+      email,
+      school
+    }) => {
+      let intern = new Intern(name, id, school, email);
+      employeeArray.push(intern)
+      getEmployee();
+    })
+}
+
+init();
