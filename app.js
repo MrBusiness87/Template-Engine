28ed95c69c57b2d2570a5a42db9025f644f1be42
MrBusiness87 <bbartula87@gmail.com>
2020-07-25 13:05:11 -0400

    07/25/2020-more changes have questions displaying tests all positive.

--- app.js
+++ app.js
@@ -1,115 +1,53 @@
-const inquirer = require('inquirer');
-
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-const inquire = inquirer.prompt({
-  defineEmployee: [{
-    type: "list",
-    message: "What is your position?",
-    name: "userChoice",
-    choices: ["Engineer", "Intern", "Manager"]
-  }],
-  employeeQuestions: [{
+var inquirer = require('inquirer');
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+let allAnswers = []
+const gatherInfo = async () => {
+  const answers = await inquirer.prompt([{
       type: "input",
-      message: "What is your name?",
+      message: "Enter Name:",
       name: "name"
     },
     {
       type: "input",
-      message: "What is your employee ID number?",
+      message: "Enter Employee ID Number:",
       name: "id",
     },
     {
       type: "input",
-      message: "Please enter a valid email:",
+      message: "Enter Email:",
       name: "email",
-    }
-
-  ],
-  managerQuestions: [{
-    type: "input",
-    message: "Office Number",
-    name: "officeNumber",
-  }],
-
-  engineerQuestions: [{
-    type: "input",
-    message: "GitHub Username",
-    name: "githubUsername"
-  }],
-
-  internQuestions: [{
-    type: "input",
-    message: "School",
-    name: "school"
-  }]
-});
+    },
+    {
+      type: "checkbox",
+      message: "Position",
+      name: "position",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+    {
+      type: "confirm",
+      name: "continue",
+      message: "Do you want to enter another employee?",
+      default: true,
+    },
+  ]);
+  console.log(answers)
+  allAnswers = [...allAnswers, answers]
+  if (answers.continue) {
+    gatherInfo()
+  }
+  console.log(allAnswers)
+};
+gatherInfo()
 
 const htmlRenderer = require('./lib/htmlRenderer');
 
-const employeeArray = [];
-
-function init() {
-  inquire(employeeQuestions.concat(managerQuestions))
-    .then(({
-      name,
-      id,
-      email,
-      officeNumber
-    }) => {
-      let manager = new Manager(name, id, officeNumber, email);
-      employeeArray.push(manager);
-      getEmployee();
-    })
-}
-
-function getEmployee() {
-  inquire(defineEmployee)
-    .then(({
-      userChoice
-    }) => {
-      switch (userChoice) {
-        case 'Engineer':
-          getEngineer();
-          break;
-        case 'Intern':
-          getIntern();
-          break;
-        case 'Complete':
-          htmlRenderer(employeeArray);
-          break;
-      }
-    })
-}
-
-function getEngineer() {
-  inquire(employeeQuestions.concat(engineerQuestions))
-    .then(({
-      name,
-      id,
-      email,
-      githubUsername
-    }) => {
-      let engineer = new Engineer(name, id, githubUsername, email);
-      employeeArray.push(engineer);
-      getEmployee();
-    })
-}
-
-function getIntern() {
-  inquire(employeeQuestions.concat(internQuestions))
-    .then(({
-      name,
-      id,
-      email,
-      school
-    }) => {
-      let intern = new Intern(name, id, school, email);
-      employeeArray.push(intern)
-      getEmployee();
-    })
-}
-
-init();
+function htmlRenderer(allAnswers);
